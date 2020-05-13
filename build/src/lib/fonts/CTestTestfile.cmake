eccc18247ba376d1d1f0025010b0e21739c39aa4
RazdolbayOne <55252915+RazdolbayOne@users.noreply.github.com>
2020-05-13 10:13:33 +0300

    added config and fixed advert without build dir

--- build/src/lib/fonts/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Desktop/PP/SmartRTU/src/lib/fonts
-# Build directory: /home/pi/Desktop/PP/SmartRTU/build/src/lib/fonts
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/lib/json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Desktop/PP/SmartRTU/src/lib/json
-# Build directory: /home/pi/Desktop/PP/SmartRTU/build/src/lib/json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
